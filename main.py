457fc7a25a29c18af2879010a0a92ff0210824f9
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-07 23:18:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+side = random.randint(0, 1)
+
+if side == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
+
